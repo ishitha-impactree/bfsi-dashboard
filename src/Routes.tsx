478b946d7dfc2483e0324bcbb9ded00526dfafd5
Ramkumar <ramkumar@impactree.ai>
2025-10-01 01:36:02 +0530

    Default landing route page changes done

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,23 +7,23 @@ import BenchmarksPage from './pages/Benchmarks';
 import ReportsPage from './pages/Reports';
 import Risks from './pages/Risks';
 import PortfolioPage from './pages/Portfolio';
-import CompaniesStatistics from './pages/Companies'; 
-import SignInForm from './pages/SignInForm/SignInForm'; 
+import CompaniesStatistics from './pages/Companies';
+import SignInForm from './pages/SignInForm/SignInForm';
 // import SignUpForm from 'pages/SignUpForm/SignUpForm';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CockpitPage />} />
+        <Route path="/" element={<SignInForm />} />
         <Route path="/cockpit" element={<CockpitPage />} />
         <Route path="/sectors" element={<SectorsPage />} />
         <Route path="/benchmarks" element={<BenchmarksPage />} />
-        <Route path="/reports" element={<ReportsPage />}/>
+        <Route path="/reports" element={<ReportsPage />} />
         <Route path="/risks" element={<Risks />} />
-        <Route path="/portfolio" element={<PortfolioPage />}/>
-        <Route path="/companies" element={<CompaniesStatistics />}/>
-        <Route path="/sign-in" element={<SignInForm />}/> 
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/companies" element={<CompaniesStatistics />} />
+        <Route path="/sign-in" element={<SignInForm />} />
         {/* <Route path="/sign-up" element={<SignUpForm />}/> */}
         <Route path="*" element={<h1>Page not found</h1>} />
       </Routes>
